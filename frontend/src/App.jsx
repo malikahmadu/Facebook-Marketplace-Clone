2b99e83c82165bcc9576e3bd428d815a35e18e0e
Malik <malikahmadu@gmail.com>
2025-08-23 00:10:21 -0500

    implemented starter routing paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Profile from "./pages/Profile";
+import Chat from "./pages/Chat";
+import CreateListing from "./pages/CreateListing";
+
+export default function App() {
   return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/create-listing" element={<CreateListing />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
